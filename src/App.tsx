2a5aee41c04870159139bb00a0b508009f13de9b
KAYOUNG KIM <lvyest.dev@gmail.com>
2026-04-03 03:48:41 +0900

    Feat: Board List 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react'
 import './App.css'
 import { appContainer, buttons, board } from './App.css'
+import BoardList from './components/BoardList/BoardList'
 
 function App() {
 
+  const [activeBoardId, setActiveBoardId] = useState('board-0');
+  
   return (
     <div className={appContainer}>
+      <BoardList activeBoardId={activeBoardId} setActiveBoardId={setActiveBoardId}/>
       <div className={board}>
-
       </div>
       <div className={buttons}>
         <button>
