4e5f912fade892b47cd1438487d128a396add3d5
KAYOUNG KIM <lvyest.dev@gmail.com>
2026-04-08 17:27:28 +0900

    feat: LoggerModal, LogItem 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-import { appContainer, buttons, board } from './App.css'
+import { appContainer, buttons, board, deleteBoardButton, loggerButton } from './App.css'
 import BoardList from './components/BoardList/BoardList'
 import ListsContainer from './components/ListsContainer/ListsContainer';
 import { useTypedSelector } from './hooks/redux';
 import EditModal from './components/EditModal/EditModal';
+import LoggerModal from './components/LoggerModal/LoggerModal';
 
 function App() {
-
+  const [isLoggerOpen, setIsLoggerOpen] = useState(false);
   const [activeBoardId, setActiveBoardId] = useState('board-0');
   const modalActive = useTypedSelector(state => state.boards.modalActive)
   const boards = useTypedSelector(state => state.boards.boardArray)
@@ -17,17 +18,18 @@ function App() {
 
   return (
     <div className={appContainer}>
+      {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen}/> : null}
       {modalActive ? <EditModal /> : null}
       <BoardList activeBoardId={activeBoardId} setActiveBoardId={setActiveBoardId}/>
       <div className={board}>
         <ListsContainer lists={lists} boardId={getActiveBoard.boardId}/>
       </div>
       <div className={buttons}>
-        <button>
+        <button className={deleteBoardButton}>
           이 게시판 삭제하기
         </button>
-        <button>
-           
+        <button className={loggerButton} onClick={()=>setIsLoggerOpen(!isLoggerOpen)}>
+           {isLoggerOpen ? "활동 목록 숨기기" : "활동 목록 보이기" }
         </button>
       </div>
     </div>
